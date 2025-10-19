770b25a2a525ac3a5a5a4ca0bedf103c24deb571
Shuiyunshen66 <144821057+Shuiyunshen66@users.noreply.github.com>
2025-10-20 01:29:44 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,16 @@
-//
-//
-//
+//202511716118
+//1441913365@qq.com
+//杨雨鑫
 #include <stdio.h>
+
 int main()
 {
-    printf("Hello World");
+    float a = 0.0;
+    printf("请输入摄氏温度:");
+    scanf("%f",&a);
+    float b = a * (9.0f / 5.0f) +32.0f;
+    printf("华氏温度为：%.1f\n",b);
+
     return 0;
+    
 }
