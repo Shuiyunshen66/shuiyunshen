57099be4eeab2866bb849b94dce56aa0cad89469
Shuiyunshen66 <144821057+Shuiyunshen66@users.noreply.github.com>
2025-10-31 20:22:20 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,16 +1,48 @@
 //202511716118
 //1441913365@qq.com
 //杨雨鑫
-#include <stdio.h>
 
+#include<stdio.h>
 int main()
+
 {
-    float a = 0.0;
-    printf("请输入摄氏温度:");
-    scanf("%f",&a);
-    float b = a * (9.0f / 5.0f) +32.0f;
-    printf("华氏温度为：%.1f\n",b);
+    int a;
+    int prime = 1;//若是质数，则prime输出为1
+
+    printf("请输入密钥：");
+    scanf("%d",&a);
+
+    if (a <= 0 || a >= 50)
+    {
+        printf("输入无效，请输入小于50的正整数\n");
+        return 0;
+    }
+
+    if (a == 1)
+    {
+        prime = 0 ;
+    }
+    else 
+    {
+        for (int i = 2; i < a; i++)
+        {
+            if (a % i == 0) //若a能被i整除，说明不是质数，prime输出为0
+            {
+                prime = 0;
+                break;
+            }
+        }
+    }
+
+    if (prime)
+    {
+        printf("密钥安全，密码设置成功\n");
+    }
+    else 
+    {
+        printf("密钥不安全，请重新输入\n");
+    }
 
     return 0;
-    
+
 }
