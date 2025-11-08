c1b2aa74d3cf1cb94797f661b9eb2a1889d4289a
Shuiyunshen66 <144821057+Shuiyunshen66@users.noreply.github.com>
2025-11-08 14:32:22 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -3,46 +3,36 @@
 //杨雨鑫
 
 #include<stdio.h>
+void reput(int arr[],int j)
+{
+    int temp;
+    for(int i = 0;i < j/2;i ++)
+    {
+        temp = arr[i];
+        arr[i] = arr[j - 1 - i];
+        arr[j - 1 - i] = temp;
+    }
+} 
+
 int main()
 {
-    int a;
-    int prime = 1;//若是质数，则prime输出为1
-    int i = 2;
-
-    printf("请输入密钥：");
-    scanf("%d",&a);
-
-    if (a <= 0 || a >= 50)
+    int arr[5];
+    for(int i = 0;i < 5;i ++)
     {
-        printf("输入无效，请输入小于50的正整数\n");
-        return 0;
+    scanf("%d",&arr[i]);
     }
 
-    if (a == 1)
-    {
-        prime = 0 ;
-    }
-    else
-    {
-        while(i < a)
-        {
-            if(a % i == 0)
-            {
-                prime = 0;
-                break;
-            }
-            i ++;
-        }
-    }
-    if (prime)
-    {
-        printf("密钥安全，密码设置成功\n");
-    }
-    else 
-    {
-        printf("密钥不安全，请重新输入\n");
-    }
+reput(arr , 5);
 
-    return 0;
+for(int i = 0;i < 5;i ++)
+{
+    printf("%d",arr[i]);
+    if (i < 4)
+    {
+        printf(" ");
+    }
+}
+
+return 0;
 
 }
