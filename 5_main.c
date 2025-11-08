b54a58dd62b2770b983cf3f523ddaeca8aa4e7fd
Shuiyunshen66 <144821057+Shuiyunshen66@users.noreply.github.com>
2025-11-08 14:33:40 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -3,44 +3,19 @@
 //杨雨鑫
 
 #include<stdio.h>
-int main()
-
+int times(int a1,int an,int step)
 {
-    int i = 0;
-    int b = 0;
-    int arr[5];
+    int n = (an - a1) / step + 1;
+    int sum = n*(a1 + an) / 2;
+    return sum;
+}
 
-    printf("请输入四个整数：");
+int main()
+{
+    int result = times(1,100,1);
 
-    i = 0;
-    while(i < 4)
-    {
-        scanf("%d",&arr[i]);//读取前四位数据
-        i ++;
-    }
-
-
-    i = 0;
-    b = 0;
-    while(i < 4)
-    {
-        b += arr[i];//计算
-        i++;
-    }
-    arr[4] = b;
-
-    i = 0;
-    while(i < 5)
-    {
-        printf("%d",arr[i]);
-        if (i < 4)
-        {
-            printf(" ");
-        }
-        i++;
-    }
-
-    printf("\n");
+    printf("%d",result);
 
     return 0;
+    
 }
