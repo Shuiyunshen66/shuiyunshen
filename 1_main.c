1387881eed3e302ffa8348dc1158cfb9dadc55ef
Shuiyunshen66 <144821057+Shuiyunshen66@users.noreply.github.com>
2025-11-08 14:31:11 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,43 +4,28 @@
 
 #include<stdio.h>
 int main()
-
 {
-    int a;
-    int prime = 1;//若是质数，则prime输出为1
-
-    printf("请输入密钥：");
-    scanf("%d",&a);
-
-    if (a <= 0 || a >= 50)
+    int arr[3][3];
+    for(int i = 0;i < 3;i ++)
     {
-        printf("输入无效，请输入小于50的正整数\n");
-        return 0;
-    }
-
-    if (a == 1)
-    {
-        prime = 0 ;
-    }
-    else 
-    {
-        for (int i = 2; i < a; i++)
+        for(int j = 0; j < 3; j ++)
         {
-            if (a % i == 0) //若a能被i整除，说明不是质数，prime输出为0
+            scanf("%d",&arr[i][j]);
+        } 
+    }
+
+    printf("");
+    for(int i = 0;i < 3; i ++)
+    {
+        for(int j = 0;j < 3; j ++)
+        {
+            printf("%d",arr[i][j]);
+            if(j < 2)
             {
-                prime = 0;
-                break;
+                printf(" ");
             }
         }
-    }
-
-    if (prime)
-    {
-        printf("密钥安全，密码设置成功\n");
-    }
-    else 
-    {
-        printf("密钥不安全，请重新输入\n");
+        printf("\n");
     }
 
     return 0;
